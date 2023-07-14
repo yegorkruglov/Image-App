208a3f48e5dd7dc45f530686a8d9df0c632ce6ab
Egor Kruglov <yegorkruglov@gmail.com>
2023-07-14 18:53:47 +0300

    +searchBar and custom Image cell

--- Image App/ViewControllers/CollectionViewController.swift
+++ Image App/ViewControllers/CollectionViewController.swift
@@ -10,33 +10,38 @@ import UIKit
 final class CollectionViewController: UICollectionViewController {
     
     // MARK: - Private Properties
-    private let reuseIdentifier = "Cell"
-    
     private let sectionInsets = UIEdgeInsets(top: 20, left: 20.0, bottom: 20, right: 20.0)
-    
     private let itemsPerRow: CGFloat = 2
-    
+    private var searchBar: UISearchController = {
+        let searchBar = UISearchController()
+        searchBar.searchBar.placeholder = "Search images"
+        searchBar.searchBar.searchBarStyle = .minimal
+        return searchBar
+    }()
+
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "ImageCell")
         
         title = "Image App"
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchBar
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchBar.searchResultsUpdater = self
+        
+        
     }
-    
-    
-    // MARK: - UICollectionViewDataSource
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
+}
+
+// MARK: - UICollectionViewDataSource
+extension CollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath)
         cell.backgroundColor = .blue
         
         return cell
@@ -55,10 +60,19 @@ extension CollectionViewController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return sectionInsets
+        sectionInsets
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return sectionInsets.left
+        sectionInsets.left
     }
+}
+
+// MARK: - UISearchResultsUpdating
+extension CollectionViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text else{return}
+    }
+    
+    
 }
---
+++ Image App/Views/ImageCell.swift
@@ -0,0 +1,43 @@
+//
+//  ImageCell.swift
+//  Image App
+//
+//  Created by Egor Kruglov on 14.07.2023.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imageView)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension ImageCell{
+    func configureUI(){
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        imageView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+    }
+    
+    func updateCell(posterURL: String?){
+        
+        
+    }
+}
