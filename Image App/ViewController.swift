80b1cb8c3d6ffc44b0ad577f23b7ef1912cd1980
Egor Kruglov <yegorkruglov@gmail.com>
2023-07-14 15:53:39 +0300

    Initial Commit

---
+++ Image App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image App
+//
+//  Created by Egor Kruglov on 14.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
